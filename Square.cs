24ad62f52439a727208df60b533be501a2dd7764
Ana Isakov <aisakov8709@conestogac.on.ca>
2024-10-17 00:07:24 -0400

    Square class set up
    All properties and inheritance set up

---
+++ Square.cs
@@ -0,0 +1,30 @@
+﻿// Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2_study
+{
+	/// <summary>
+	/// Represents a Square in the "Black Hole" or "QGame" grid (a subclass of PictureBox)
+	/// </summary>
+	public class Square : PictureBox
+	{
+		public static string sPattern; // static pattern in square
+		private DesignerForm designerForm; // variable to represent the designer form
+
+		/// <summary>
+		/// Constructor that takes the designer form as parameter
+		/// </summary>
+		/// <param name="designerForm"></param>
+		public Square(DesignerForm designerForm)
+		{
+			this.designerForm = designerForm;
+			this.BorderStyle = BorderStyle.FixedSingle; // add a border to each picturebox
+			this.SizeMode = PictureBoxSizeMode.StretchImage; // sets image size mode
+		}
+	}
+}
